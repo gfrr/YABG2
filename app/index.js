baf1e209143c59113e6d13a4527f682abd363485
Giuseppe Fierro <fierro.gpp@gmail.com>
2017-08-18 18:29:21 +0200

    first commit

---
+++ app/index.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+require("./index.css");
+
+
+ReactDOM.render(<App/>, document.getElementById('app'));
